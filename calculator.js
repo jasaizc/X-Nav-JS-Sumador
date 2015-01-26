d3936d6db1862cb5228f0d064eabf08d69057454
Jesus Alberto Saiz Cano <jasc@lambda25.aulas.gsyc.es>
2015-01-26 14:48:41 +0100

    Primer ejercicio Calculadora

--- calculator.js
+++ calculator.js
@@ -2,6 +2,12 @@ function changer (id, newValue) {
 var element = document.getElementById(id);
 element.innerHTML = newValue;
 }
+
+
+
 function sumar(){
-changer("op", "5+3 = 8")
+    p = document.getElementById('op');
+    resultado = p.innerHTML.split("+",2);
+    result= parseInt(resultado[0]) + parseInt(resultado[1])
+    changer("op", result)
 }
