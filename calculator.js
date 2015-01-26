1c0eb152e103195dc28c584c10b2bb0cb4090b4c
Jesus Alberto Saiz Cano <jasc@lambda25.aulas.gsyc.es>
2015-01-26 14:55:41 +0100

    Primer ejercicio Calculadora aleatoria

--- calculator.js
+++ calculator.js
@@ -11,3 +11,17 @@ function sumar(){
     result= parseInt(resultado[0]) + parseInt(resultado[1])
     changer("op", result)
 }
+
+function sumarAleatoria(){
+    p = document.getElementById('Aleatorio');
+    primervalor = aleatorio(0,1000000);
+    segundovalor = aleatorio(0,1000000);
+    result= primervalor + segundovalor;
+    changer("Aleatorio", primervalor + " + " + segundovalor + " = " + result)
+}
+function aleatorio(inferior,superior){
+    var numPosibilidades = superior - inferior
+    var aleat = Math.random() * numPosibilidades
+    aleat = Math.round(aleat)
+    return parseInt(inferior) + aleat
+} 
